e31c40f81d4a13a9ad00315d8d12304b4cc000ef
Maximilian Held <info@maxheld.de>
2017-04-11 18:51:35 +0200

    dropzone tryout

--- server.R
+++ server.R
@@ -4,4 +4,13 @@ shinyServer(function(input, output) {
   output$text1 <- shiny::renderText({
     paste("Currently being dragged item:", input$dropzone_dragging)
   })
+  output$text3 <- shiny::renderText({
+    paste("Currently being dragged item:", input$item1_offset)
+  })
+  output$text2 <- shiny::renderText({
+    paste("Currently being dragged item:", input$item2_position)
+  })
+  output$text4 <- shiny::renderText({
+    paste("Item on dropzone:", input$dropzone_dropped)
+  })
 })
--- ui.R
+++ ui.R
@@ -9,14 +9,33 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(
       "sidebar panel",
-      shiny::textOutput(outputId = "text1")
+      #shiny::textOutput(outputId = "text1"),
+      shiny::textOutput(outputId = "text2"),
+      shiny::textOutput(outputId = "text3"),
+      shiny::textOutput(outputId = "text4")
     ),
     mainPanel(
       strong("main panel"),
       jqui_draggabled(tag = shiny::textInput(inputId = 'item1', label = 'Item 1')),
       jqui_draggabled(tag = shiny::textInput(inputId = 'item2', label = 'Item 2')),
       jqui_draggabled(tag = shiny::textInput(inputId = 'item3', label = 'Item 3')),
-      jqui_droppabled(tag = shiny::textInput(inputId = 'dropzone', label = 'Drop Zone'))
+      jqui_droppabled(tag = shiny::textInput(inputId = 'dropzone', label = 'Drop Zone'),
+                      options = list(
+                        shiny = list(
+                          # By default, draggable element has a shiny input value showing the element's
+                          # position (relative to the parent element). Here, another shiny input
+                          # value is added. It gives the element's offset (position relative to the
+                          # document). Using input$foo_offset to get access to it .
+                          dropped = list(
+                            # return the initiated offset value when the draggable is created
+                            # dropcreate = NA,
+                            # update the offset value while dragging
+                            # drop = p("foobar")
+                            drop = htmlwidgets::JS('function(event, ui) { return $(event.result).droppable(); }')
+                          )
+                        )
+                     )
+      )
     )
   )
 ))

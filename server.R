8c987cac0aa38b7d74362b5449d74a4efe84d48c
Maximilian Held <info@maxheld.de>
2017-04-12 23:15:35 +0200

    the grid works!!!
    let there be light!

--- server.R
+++ server.R
@@ -6,10 +6,13 @@ shinyServer(function(input, output) {
   output$text1 <- shiny::renderText({
     paste("Item on dropzone:", input$dropzone_dropped)
   })
+  output$text1 <- shiny::renderText({
+    paste("Ich bin in Zelle 0a links oben:", input$'0a_dropped')
+  })
 
   # INTERACTIONS for items and cells
-  jqui_draggable(selector = '.item')
-  jqui_droppable(selector = '.cell',
+  jqui_draggable(selector = '.hexagon-item')
+  jqui_droppable(selector = '.hexagon',
                  options = list(
                    shiny = list(
                      dropped = list(

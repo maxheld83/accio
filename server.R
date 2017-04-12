48eb19b740532b5ee61d6c96ddcc042c5774f628
Maximilian Held <info@maxheld.de>
2017-04-12 23:03:58 +0200

    import html, adopt class interactions

--- server.R
+++ server.R
@@ -1,8 +1,20 @@
 library(shiny)
 library(shinyjqui)
+library(readr)
 
 shinyServer(function(input, output) {
   output$text1 <- shiny::renderText({
     paste("Item on dropzone:", input$dropzone_dropped)
   })
+
+  # INTERACTIONS for items and cells
+  jqui_draggable(selector = '.item')
+  jqui_droppable(selector = '.cell',
+                 options = list(
+                   shiny = list(
+                     dropped = list(
+                       drop = htmlwidgets::JS('function(event, ui) { return ui.draggable.attr("id");}')
+                     )
+                   )
+                 ))
 })
--- ui.R
+++ ui.R
@@ -11,20 +11,15 @@ shinyUI(fluidPage(
       shiny::textOutput(outputId = "text1")
     ),
     mainPanel(
+      h3("Items"),
+      tags$div(
+        HTML(read_file(file = "www/index.html"))
+      ),
+      h3("Test"),
       strong("main panel"),
-      jqui_draggabled(div(id = 'item-1', class = 'item',
-          'I am Item 1.')),
-      jqui_draggabled(div(id = 'item-2', class = 'item',
-          'I am Item 2.')),
-      jqui_droppabled(tag = div(id = 'dropzone', class = 'item',
-                          'I am the dropzone.'),
-                      options = list(
-                        shiny = list(
-                          dropped = list(
-                            drop = htmlwidgets::JS('function(event, ui) { return ui.draggable.attr("id");}')
-                          )
-                        )
-                      ))
+      div(id = 'item-1', class = 'item', 'I am Item 1.'),
+      div(id = 'item-2', class = 'item', 'I am Item 2.'),
+      div(id = 'dropzone', class = "cell", "I am the dropzone")
     )
   )
 ))

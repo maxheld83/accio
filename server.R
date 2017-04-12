d9ab96e2c44117fdaf416d3ce3b67b0d810608c4
Maximilian Held <info@maxheld.de>
2017-04-12 18:12:49 +0200

    add working proof of concept
    this is now without inputText and now works as expected

--- server.R
+++ server.R
@@ -1,16 +1,8 @@
 library(shiny)
+library(shinyjqui)
 
 shinyServer(function(input, output) {
   output$text1 <- shiny::renderText({
-    paste("Currently being dragged item:", input$dropzone_dragging)
-  })
-  output$text3 <- shiny::renderText({
-    paste("Currently being dragged item:", input$item1_offset)
-  })
-  output$text2 <- shiny::renderText({
-    paste("Currently being dragged item:", input$item2_position)
-  })
-  output$text4 <- shiny::renderText({
     paste("Item on dropzone:", input$dropzone_dropped)
   })
 })
--- ui.R
+++ ui.R
@@ -1,7 +1,3 @@
-library(shinyjqui)
-library(shiny)
-library(ggplot2)
-
 shinyUI(fluidPage(
   includeJqueryUI(),
   titlePanel("title panel"),
@@ -9,33 +5,23 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(
       "sidebar panel",
-      #shiny::textOutput(outputId = "text1"),
-      shiny::textOutput(outputId = "text2"),
-      shiny::textOutput(outputId = "text3"),
-      shiny::textOutput(outputId = "text4")
+      shiny::textOutput(outputId = "text1")
     ),
     mainPanel(
       strong("main panel"),
-      jqui_draggabled(tag = shiny::textInput(inputId = 'item1', label = 'Item 1')),
-      jqui_draggabled(tag = shiny::textInput(inputId = 'item2', label = 'Item 2')),
-      jqui_draggabled(tag = shiny::textInput(inputId = 'item3', label = 'Item 3')),
-      jqui_droppabled(tag = shiny::textInput(inputId = 'dropzone', label = 'Drop Zone'),
+      jqui_draggabled(div(id = 'item-1', class = 'item',
+          'I am Item 1.')),
+      jqui_draggabled(div(id = 'item-2', class = 'item',
+          'I am Item 2.')),
+      jqui_droppabled(tag = div(id = 'dropzone', class = 'item',
+                          'I am the dropzone.'),
                       options = list(
                         shiny = list(
-                          # By default, draggable element has a shiny input value showing the element's
-                          # position (relative to the parent element). Here, another shiny input
-                          # value is added. It gives the element's offset (position relative to the
-                          # document). Using input$foo_offset to get access to it .
                           dropped = list(
-                            # return the initiated offset value when the draggable is created
-                            # dropcreate = NA,
-                            # update the offset value while dragging
-                            # drop = p("foobar")
-                            drop = htmlwidgets::JS('function(event, ui) { return $(event.result).droppable(); }')
+                            drop = htmlwidgets::JS('function(event, ui) { return ui.draggable.attr("id");}')
                           )
                         )
-                     )
-      )
+                      ))
     )
   )
 ))

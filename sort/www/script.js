6a37131da0fb24c1a11740e7e71c9c3c822d4528
Verena <VerenaKasztantowicz@gmail.com>
2017-06-28 12:06:34 +0200

    Set grid container (adapted to window height)

--- sort/www/script.js
+++ sort/www/script.js
@@ -1,3 +1,11 @@
+// set height of .grid-container depending on window (full screen)
+$(function(){
+  $(".grid-container").css({ height: $(window).innerHeight() });
+  $(window).resize(function(){
+    $(".grid-container").css({ height: $(window).innerHeight() });
+  });
+});
+
 $(function() {
   $(".draggable").draggable({
     // option cursorAt is set dynamically in the below

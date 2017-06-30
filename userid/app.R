661c4309978c0f2e9cfb43f6a169693ff748f465
Maximilian Held <info@maxheld.de>
2017-06-30 23:54:43 +0200

    this works, but creates duplicates

--- userid/app.R
+++ userid/app.R
@@ -97,8 +97,64 @@ ui <- fillPage(
   )
 )
 
+
+desirable <- matrix(data = NA,
+                    nrow = 5,
+                    ncol = 9,
+                    dimnames = list(rows = c("a", "b", "c", "d", "e"),
+                                    columns = c("01", "02", "03", "04", "05", "06", "07", "08", "09")))
+
+
+make_newsort <- function(cells, emptymat) {
+  # emptymat <- desirable
+  # cells <- list(b02_drop = "foo", a03_drop = "bar", e09_drop = "lirum")
+  # set all to NA
+  emptymat[,] <- NA
+  newmat <- emptymat
+  for (row_i in rownames(emptymat)) {
+    for (col_i in 1:ncol(emptymat)) {
+      thisel <- paste0(row_i, "0", col_i, "_", "drop")
+      if (!is.null(cells[[thisel]])) {
+        newmat[row_i, col_i] <- cells[[thisel]]
+      }
+    }
+  }
+  return(newmat)
+}
+
+# test_oldmat <- structure(c(NA, NA, NA, NA, NA, NA, "foo", NA, NA, NA, "bar",
+#                            NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA,
+#                            NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA,
+#                            NA, "lirum"), .Dim = c(5L, 9L), .Dimnames = structure(list(rows = c("a",
+#                                                                                                "b", "c", "d", "e"), columns = c("01", "02", "03", "04", "05",
+#                                                                                                                                 "06", "07", "08", "09")), .Names = c("rows", "columns")))
+# test_newmat <- structure(c(NA, NA, NA, NA, NA, NA, "foo", NA, NA, NA, "bar",
+#                            NA, NA, NA, NA, NA, NA, "foo", NA, NA, NA, NA, NA, NA, NA, NA, NA,
+#                            NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA,
+#                            NA, "lirum"), .Dim = c(5L, 9L), .Dimnames = structure(list(rows = c("a",
+#                                                                                                "b", "c", "d", "e"), columns = c("01", "02", "03", "04", "05",
+#                                                                                                                                 "06", "07", "08", "09")), .Names = c("rows", "columns")))
+
+update_sort <- function(oldsort, newsort) {
+  # newsort <- test_newmat
+  # oldsort <- test_oldmat
+  for (row_i in rownames(newsort)) {
+    for (col_i in 1:ncol(newsort)) {
+      current_item <- newsort[row_i, col_i]
+      # current_item <- "as"
+      # current_item <- "foo"
+      if (sum(current_item == newsort, na.rm = TRUE) > 1) {
+        newsort[which(oldsort == newsort & newsort == current_item, arr.ind = TRUE)] <- NA
+      }
+    }
+  }
+  return(newsort)
+}
+
+
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+
   # Return the UI for a modal dialog with data selection input. If 'failed' is
   # TRUE, then display a message that the previous value was invalid.
   dataModal <- function(failed = FALSE) {
@@ -227,11 +283,14 @@ server <- function(input, output) {
     },
     handlerExpr = {
       oldmat <- res$desirable
+      write.csv(x = res$desirable, file = "oldmat.csv")
       input_static <- reactiveValuesToList(input)
+      write_rds(input_static, path = "input_static.rds")
       newmat <- make_newsort(cells = input_static,
                              emptymat = res$desirable)
+      write.csv(x = newmat, file = "newmat.csv")
       res$desirable <- update_sort(oldsort = oldmat, newsort = newmat)
-      write.csv(x = res$desirable, file = "test.csv")
+      write.csv(x = res$desirable, file = "desirable.csv")
     }
   )
 

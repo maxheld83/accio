279825ffeff7a22d6893b42bf81f6b1e7a88578b
Maximilian Held <info@maxheld.de>
2017-08-15 23:00:38 +0200

    add proper validation via validate closes https://github.com/maxheld83/pensieve/issues/308
    start on https://github.com/maxheld83/pensieve/issues/308

--- server.R
+++ server.R
@@ -1,10 +1,19 @@
 shinyServer(function(input, output) {
 
   output$item_preview <- renderText(expr = {
-    validate(
-      need(expr = {input$item_handle != ""},
-           label = "Item Handle")
+    new_item <- QItemConcourse(
+      concourse = matrix(
+        data = input$item_full,
+        nrow = 1,
+        ncol = 1,
+        dimnames = list(items = input$item_handle, languages = c("english"))
+      ),
+      validate = FALSE
     )
-    input$item_handle
+
+    validate(
+      need(x = new_item, label = "Item handle or full text")
+    )
+    return(new_item)
   })
 })

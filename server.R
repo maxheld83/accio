225330832c4f689b7203fd84e6ffc546a5630f0a
Maximilian Held <info@maxheld.de>
2017-08-19 16:40:39 +0200

    change to modal design https://github.com/maxheld83/accio/issues/150

--- server.R
+++ server.R
@@ -1,22 +1,42 @@
 shinyServer(function(input, output) {
 
-  output$item_preview <- renderText(expr = {
-    input$item_render
-    isolate(expr = {
-      new_item <- QItemConcourse(
-        concourse = matrix(
-          data = input$item_full,
-          nrow = 1,
-          ncol = 1,
-          dimnames = list(items = input$item_handle, languages = c("english"))
-        ),
-        validate = FALSE
-      )
+  showModal(modalDialog(
+    title = "Enter a New Item",
+    shiny::textInput(inputId = "item_handle",
+                     label = "Item handle (researcher-facing):",
+                     placeholder = "Only letters, numbers, '.' and '_'"),
+    shiny::textAreaInput(inputId = "item_full",
+                         label = "Full item wording (participant-facing)",
+                         placeholder = "Plain text.",
+                         height = '10pc',
+                         resize = "vertical"),
+    shiny::textOutput(outputId = "item_preview"),
+    easyClose = TRUE,
+    size = "l",
+    fade = FALSE,
+    footer = tagList(
+      modalButton(label = "Cancel"),
+      actionButton(inputId = "submit", label = "Submit")
+    )
+  ))
 
-      validate(
-        need(x = new_item, label = "Item handle or full text")
-      )
-      return(new_item)
-    })
+  output$item_preview <- renderText(expr = {
+    shinyjs::disable(id = "submit")  # start with disabled button
+    req(input$item_handle, input$item_full)
+    new_item <- QItemConcourse(
+      concourse = matrix(
+        data = input$item_full,
+        nrow = 1,
+        ncol = 1,
+        dimnames = list(items = input$item_handle, languages = c("english"))
+      ),
+      validate = FALSE
+    )
+
+    validate(
+      need(x = new_item, label = "Item handle or full text")
+    )
+    shinyjs::enable(id = "submit")  # only enable button on successful test
+    return(new_item)
   })
 })
--- ui.R
+++ ui.R
@@ -1,17 +1,3 @@
 shinyUI(bootstrapPage(
-
-  shiny::textInput(inputId = "item_handle",
-                   label = "Item handle (researcher-facing):",
-                   placeholder = "Only letters, numbers, '.' and '_'"),
-
-  shiny::textAreaInput(inputId = "item_full",
-                       label = "Full item wording (participant-facing)",
-                       placeholder = "Plain text.",
-                       height = '10pc',
-                       resize = "vertical"),
-
-  shiny::actionButton(inputId = "item_render",
-                      label = "Preview"),
-
-  shiny::textOutput(outputId = "item_preview")
+  shinyjs::useShinyjs()  # Include shinyjs
 ))
